2c0a869ea90ad552446723c03c09ff6284a53501
kankan <6188916+trripy@users.noreply.github.com>
2018-02-04 16:53:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from future.standard_library import install_aliases
 install_aliases()
-import paho.mqtt.client as mqtt
+
 from urllib.parse import urlparse, urlencode
 from urllib.request import urlopen, Request
 from urllib.error import HTTPError
@@ -15,37 +15,80 @@ from flask import make_response
 
 # Flask app should start in global layout
 app = Flask(__name__)
+
+
 @app.route('/webhook', methods=['POST'])
-uni_data=''
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("kankan")
-
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    processRequest(msg.payload)
-
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect("18.216.139.115", 1883, 60)
-
-
-
-
-
 def webhook():
     req = request.get_json(silent=True, force=True)
+
     print("Request:")
     print(json.dumps(req, indent=4))
-    #res = processRequest(req)
-    speech = "Your Vehicle is in "+"Bellandur"
-    print (speech)
-    res= {
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    if req.get("result").get("action") != "yahooWeatherForecast":
+        return {}
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
+    res = makeWebhookResult(data)
+    return res
+
+
+def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+
+
+def makeWebhookResult(data):
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+
+    # print(json.dumps(item, indent=4))
+
+    speech = "Today the weather in " + location.get('city') + ": " + condition.get('text') + \
+             ", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
+    print("Response:")
+    print(speech)
+
+    return {
         "speech": speech,
         "displayText": speech,
         # "data": data,
@@ -53,43 +96,10 @@ def webhook():
         "source": "apiai-weather-webhook-sample"
     }
 
-    res = json.dumps(res, indent=4)
-    # print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    print(r)
-    return r
-
-
-def processRequest(reh):
-   # if req.get("result").get("action") != "yahooWeatherForecast":
-   #     return {}
-    baseurl = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyA_ASQP-QLxu8N9DOFjn5UBogVdfwBGxF0&sensor=false&language=en&latlng="
-    #yql_query = makeYqlQuery(req)
-    #if yql_query is None:
-    #    return {}
-    yql_url = baseurl +str(reh.decode("utf-8"))+ "&format=json"
-    print(yql_url)
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
-
-
-def makeWebhookResult(data):
-    rest=data.get("results")[0].get("formatted_address")
-    if rest is None:
-        return{}
-    print(rest)
-    uni_data=rest
-    
-
 
 if __name__ == '__main__':
-    client.loop_forever()
     port = int(os.getenv('PORT', 5000))
 
     print("Starting app on port %d" % port)
 
-    app.run(debug=False, port=port, host='0.0.0.0')
-    
+app.run(debug=False, port=port, host='0.0.0.0')
